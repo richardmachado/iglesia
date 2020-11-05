05149f24b9e663fe8b85538550790f1a54df223b
Richard Machado <mmachado.richard@gmail.com>
2020-11-05 09:54:57 -0800

    dynamically running bible books in spanish bible instead of hard coding, moved them to a separate file and mapping over them as key value pairs

--- src/Components/AntiguoTestamento.js
+++ src/Components/AntiguoTestamento.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import styled from 'styled-components';
 import * as ReactBootStrap from 'react-bootstrap';
+import { libros_de_biblia } from "./BibleBooks/librosantiguos";
 
 const Chapter = styled.div `
 width: 80%;
@@ -45,7 +46,7 @@ function Antiguo() {
         .then(response => {
           setForms([response.data.data]);
           setLoading(true);
-          console.log(response)
+
         })
         .catch(err => {
           console.log(err);
@@ -65,6 +66,7 @@ function Antiguo() {
             type="text"
             onChange={event => handleChange(event)}
             form="chapter">
+            
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -222,45 +224,8 @@ function Antiguo() {
          <select name="book"
             onChange={e => handleSubmit(e)}
             form="book">
-            <option value="GEN">Génesis</option>
-            <option value="EXO">Éxodo</option>
-            <option value="LEV">Levítico</option>
-            <option value="NUM">Números</option>
-            <option value="DEU">Deuteronomio</option>
-            <option value="JOS">Josué</option>
-            <option value="JDG">Jueces</option>
-            <option value="RUT">Rut</option>
-            <option value="1SA">1 Samuel</option>
-            <option value="2SA">2 Samuel</option>
-            <option value="1KI">1 Reyes</option>
-            <option value="2KI">2 Reyes</option>
-            <option value="1CH">1 Crónicas</option>
-            <option value="2CH">2 Crónicas</option>
-            <option value="EZR">Esdras</option>
-            <option value="NEH">Nehemías</option>
-            <option value="EST">Ester</option>
-            <option value="JOB">Job</option>
-            <option value="PSA">Salmos</option>
-            <option value="PRO">Proverbios</option>
-            <option value="ECC">Eclesiastés</option>
-            <option value="SNG">Cantares</option>
-            <option value="ISA">Isaías</option>
-            <option value="JER">Jeremías</option>
-            <option value="LAM">Lamentaciones</option>
-            <option value="EZK">Ezequiel</option>
-            <option value="DAN">Daniel</option>
-            <option value="HOS">Oseas</option>
-            <option value="JOL">Joel</option>
-            <option value="AMO">Amós</option>
-            <option value="OBA">Abdía</option>
-            <option value="JON">Jonás</option>
-            <option value="MIC">Miqueas</option>
-            <option value="NAM">Nahúm</option>
-            <option value="HAB">Habacuc</option>
-            <option value="ZEP">Sofonias</option>
-            <option value="HAG">Hageo</option>
-            <option value="ZEC">Zacarías</option>
-            <option value="MAL">Malaquias</option>
+              {libros_de_biblia.map(({ value, label }, index) => <option value={value} >{label}</option>)}
+  
           </select>
         </label>
         {forms.map(chapterinfo => {
--- src/Components/NuevoTestamento.js
+++ src/Components/NuevoTestamento.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import styled from 'styled-components';
-
+import { libros_de_biblia } from "./BibleBooks/librosnuevos";
 
 const Chapter = styled.div `
 width: 80%;
@@ -42,7 +42,7 @@ function Nuevo() {
       axios.get(`https://api.scripture.api.bible/v1/bibles/592420522e16049f-01/chapters/${book}.${chapter}`, options)
         .then(response => {
           setForms([response.data.data]);
-          console.log(response.data)
+
         })
         .catch(err => {
           console.log(err);
@@ -99,37 +99,12 @@ function Nuevo() {
 
         <label htmlFor="book">
           Seleccione un libro
+
          <select name="book"
             onChange={e => handleSubmit(e)}
             form="book">
-            <option value="MAT">San Mateo</option>
-            <option value="MRK">San Marcos</option>
-            <option value="LUK">San Lucas</option>
-            <option value="JHN">San Juan</option>
-            <option value="ACT">Hechos</option>
-            <option value="ROM">Romanos</option>
-            <option value="1CO">1 Corintios</option>
-            <option value="2CO">1 Corintios</option>
-            <option value="GAL">Gálatas</option>
-            <option value="EPH">Efesios</option>
-            <option value="PHP">Filipenses</option>
-            <option value="COL">Colonseses</option>
-            <option value="1TH">1 Tesalonicenses</option>
-            <option value="2TH">2 Tesalonicenses</option>
-            <option value="1TI">1 Timoteo</option>
-            <option value="2TI">2 Timoteo</option>
-            <option value="TIT">Tito</option>
-            <option value="PHM">Filemón</option>
-            <option value="HEB">Hebreos</option>
-            <option value="JAS">Santiago</option>
-            <option value="1PE">1 Pedro</option>
-            <option value="2PE">2 Pedro</option>
-            <option value="1JN">1 Juan</option>
-            <option value="2JN">2 Juan</option>
-            <option value="3JN">3 Juan</option>
-            <option value="JUD">Judas</option>
-            <option value="REV">Apocalipsis</option>
-           
+              {libros_de_biblia.map(({ value, label }, index) => <option value={value} >{label}</option>)}
+  
           </select>
         </label>
    
