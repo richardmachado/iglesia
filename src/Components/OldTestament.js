fd5cbb957032c52e23a4393ac552682ee7ae284d
Richard Machado <mmachado.richard@gmail.com>
2020-11-13 21:49:19 -0800

    thanks to Edward  Blanciak for helping me make the chapter books dynamic

--- src/Components/OldTestament.js
+++ src/Components/OldTestament.js
@@ -1,115 +1,149 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import _ from 'lodash';
-import { oldtestamentbooks} from "./BibleBooks/bible_books_oldtestament";
+import { oldtestamentbooks } from './BibleBooks/bible_books_oldtestament';
 
-import { Chapter, Header, PullDownText } from '../styles2/BibleStyles';
+import {
+	Chapter,
+	Header,
+	PullDownText,
+} from '../styles2/BibleStyles';
 const API_KEY = process.env.REACT_APP_ENGLISH;
 
 // const arr = Array.apply(null, { length: oldtestamentbooks.chapters }).map(Number.call, Number);
 // console.log(arr)
 
 const renderKeys = () => {
-  var arr = [1,2,3,4,5,6,7,8,9]
-  return arr.map((val) => { 
-    return <option>{val}</option>
-  });
+	var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+	return arr.map((val) => {
+		return <option>{val}</option>;
+	});
 };
 
-console.log(renderKeys)
+console.log(renderKeys);
 
-function OldTestament () {
-    const [forms, setForms] = useState([]);
-    const [chapter, setChapter] = useState(1);
-    const [book, setBook] = useState("GENESIS");
-  
-    const handleChange = event => {
-      setChapter(event.target.value);
-    };
+function OldTestament() {
+	const [forms, setForms] = useState([]);
+	const [chapter, setChapter] = useState(1);
+	const [book, setBook] = useState('GENESIS');
+	const [numberChapters, setNumberChapters] = useState(
+		[]
+	);
 
-    const handleSubmit = e => {
-      setBook(e.target.value);
-    };
-  
-    const options = {
-        headers: {
-            "x-rapidapi-key": API_KEY,
-            "x-rapidapi-host" : "ajith-holy-bible.p.rapidapi.com",
-            "useQueryString" : "true"
-          },   
-    }
+	const handleChange = (event) => {
+		setChapter(event.target.value);
+	};
 
-  function processData() {
-    return forms[0].Output.split(/\s+(?=\d)/g);
-  }
+	const handleSubmit = (e) => {
+		setBook(e.target.value);
+	};
 
-    useEffect(() => {
-      
-    axios.get(`https://ajith-holy-bible.p.rapidapi.com/GetVerseOfaChapter?&Book=${book}&chapter=${chapter}`, options)
-          .then(response => {
-             setForms([response.data]);  
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    },[chapter, book, options]);
-    if (!forms) {
-      return <h1>Loading...</h1>;
-    }
-  
-    return (
-      <div className="forms">
-        <Header>Old Testament</Header>
+	const options = {
+		headers: {
+			'x-rapidapi-key': API_KEY,
+			'x-rapidapi-host':
+				'ajith-holy-bible.p.rapidapi.com',
+			useQueryString: 'true',
+		},
+	};
 
-  <PullDownText htmlFor="book">
-         Book of 
-         <select name="book"
-            onChange={e => handleSubmit(e)}
-            form="book">
-            
-            {
-              oldtestamentbooks.map(({ value, label }) => <option value={value} >{label}</option>)
-            }
+	function processData() {
+		return forms[0].Output.split(/\s+(?=\d)/g);
+	}
 
-          </select>
-        </PullDownText>
-        <PullDownText htmlFor="chapter">
+	useEffect(() => {
+		axios
+			.get(
+				`https://ajith-holy-bible.p.rapidapi.com/GetVerseOfaChapter?&Book=${book}&chapter=${chapter}`,
+				options
+			)
+			.then((response) => {
+				console.log(response.data);
+				setForms([response.data]);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 
-        <select
-            name="chapter"
-            type="text" 
-            onChange={event => handleChange(event)}
-            form="chapter">
-          
-            
-               { _.range(1, 150).map(value => <option key={value} value={value}>{value}</option>) }
-            
+		oldtestamentbooks.map((item) => {
+			if (item.value === book) {
+				setNumberChapters(item.chapters);
+			}
+		});
 
-            {/* { oldtestamentbooks.map(({value, chapters}) => <option key={value} value={value}>{chapters}</option>)} */}
-            
-            {/* {
-              oldtestamentbooks.map(Array(10), function (el, i) { return <option>{i}</option>} )
-            } */}
-              {/* {renderKeys} */}
-        </select>
-      </PullDownText>
-   
-       {forms.map(chapterinfo => {
-         
-         return <Chapter>
-          
-             <br></br>
-             {processData().map((data2) => (
-               <>
-                 <p>{data2}</p>
-               
-                 </>
-             ))}
-         </Chapter>
-       })} 
-      </div>
-    
-    );
-  }
-  
-  export default OldTestament;
+		// eslint-disable-next-line
+	}, [book, chapter]);
+	if (!forms) {
+		return <h1>Loading...</h1>;
+	}
+
+	// console.log(book);
+	// console.log('CURRENT CHAP: ', chapter);
+	// console.log('#chapters: ', numberChapters);
+
+	return (
+		<div className='forms'>
+			<Header>Old Testament</Header>
+
+			<PullDownText htmlFor='book'>
+				Book of
+				<select
+					name='book'
+					onChange={(e) => {
+						handleSubmit(e);
+					}}
+					form='book'
+				>
+					{oldtestamentbooks.map(
+						({ value, label }) => (
+							<option value={value}>
+								{label}
+							</option>
+						)
+					)}
+				</select>
+			</PullDownText>
+			<PullDownText htmlFor='chapter'>
+				<select
+					name='chapter'
+					type='text'
+					onChange={(event) =>
+						handleChange(event)
+					}
+					form='chapter'
+				>
+					{_.range(1, numberChapters + 1).map(
+						(value) => (
+							<option
+								key={value}
+								value={value}
+								onChange={(e) => {
+									setChapter(
+										e.target.value
+									);
+								}}
+							>
+								{value}
+							</option>
+						)
+					)}
+				</select>
+			</PullDownText>
+
+			{forms.map((chapterinfo) => {
+				return (
+					<Chapter>
+						<br></br>
+						{processData().map((data2) => (
+							<>
+								<p>{data2}</p>
+							</>
+						))}
+					</Chapter>
+				);
+			})}
+		</div>
+	);
+}
+
+export default OldTestament;
