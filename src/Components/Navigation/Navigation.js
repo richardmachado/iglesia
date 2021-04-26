7d4863725d91daff1a0b1b4163151c8bc6a95ce6
Richard Machado <mmachado.richard@gmail.com>
2021-04-26 14:40:46 -0700

    prettier

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,30 +3,31 @@ import { Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "reactstrap";
 import { Link } from "react-router-dom";
 
-import logo from "../../logo.jpg"
+import logo from "../../logo.jpg";
 
 function Navigation() {
   if (!localStorage.getItem("token")) {
     return (
       <div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand>
-                <NavLink tag={Link} to="/">
-          <img src={logo} style={{width:50, height:50, marginTop: -7}} alt="logo"/>
+          <Navbar.Brand>
+            <NavLink tag={Link} to="/">
+              <img
+                src={logo}
+                style={{ width: 50, height: 50, marginTop: -7 }}
+                alt="logo"
+              />
             </NavLink>
-            
-        </Navbar.Brand>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
               <NavLink tag={Link} to="/oldtestament">
                 Old Testament
               </NavLink>
-
               <NavLink tag={Link} to="/newtestament">
                 New Testament
               </NavLink>
-
               <NavLink tag={Link} to="/antiguotestamento">
                 Antiguo Testamento
               </NavLink>
@@ -49,23 +50,24 @@ function Navigation() {
     return (
       <div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand>
-                <NavLink tag={Link} to="/">
-          <img src={logo} style={{width:50, height:50, marginTop: -7}} alt="logo"/>
+          <Navbar.Brand>
+            <NavLink tag={Link} to="/">
+              <img
+                src={logo}
+                style={{ width: 50, height: 50, marginTop: -7 }}
+                alt="logo"
+              />
             </NavLink>
-            
-        </Navbar.Brand>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
               <NavLink tag={Link} to="/oldtestament">
                 Old Testament
               </NavLink>
-
               <NavLink tag={Link} to="/newtestament">
                 New Testament
               </NavLink>
-
               <NavLink tag={Link} to="/antiguotestamento">
                 Antiguo Testamento
               </NavLink>
